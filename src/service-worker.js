a0afd820db42b2e443e9d9fc85b8540b3d4767e4
Crusader <abdullah.mudathir@isslng.com>
2024-03-05 23:26:21 +0000

    committing dist

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,14 @@
 var cacheName = "afterschoolStore-v1";
 var cacheFiles = [
-  "index.html",
+  "./../index.html",
   //"products.js",
   "assets/store-32.jpg",
-  "assets/store-512.jpg",
-//   "s/cat-litter.jpg",
-//   "images/icon-32.png",
-//   "images/icon-512.png",
-//   "images/laser-pointer.jpg",
-//   "images/yarn.jpg",
+  "assets/store-512.png",
+  //   "s/cat-litter.jpg",
+  //   "images/icon-32.png",
+  //   "images/icon-512.png",
+  //   "images/laser-pointer.jpg",
+  //   "images/yarn.jpg",
 ];
 self.addEventListener("install", function (e) {
   console.log("[Service Worker] Install");
